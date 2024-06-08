20d43b631e163bc7f3ac3a86fd5d95d030c3790f
AndreiLumbera <165769832+AndreiLumbera@users.noreply.github.com>
2024-06-08 23:54:19 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBk-PBu89RiWL8x9Iap0MaQkxR03lWNFuc",
+  authDomain: "cruds-43776.firebaseapp.com",
+  databaseURL: "https://cruds-43776-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cruds-43776",
+  storageBucket: "cruds-43776.appspot.com",
+  messagingSenderId: "611272100373",
+  appId: "1:611272100373:web:844f29b31205dd5bfc1c1c",
+  measurementId: "G-9WTP38BESL"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
